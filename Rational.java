c170370ff622e5791339f1d2bfe52c960a9fbaae
msamatar0 <mohamedsamatar100@gmail.com>
2018-07-19 19:05:37 -0700

    Update Rational.java

--- Rational.java
+++ Rational.java
@@ -14,9 +14,7 @@ public class Rational extends Number implements Comparable<Rational>{
         long n = 0, d = 0;
         boolean one = s.matches("^-?\\d+$"),
                 two = s.matches("^-?\\d+/-?\\d+$");
-        //if(!one && !two)
-            //throw new InputMismatchException("InputMismatchException unknown command: " + s);
-        /*else*/ if(two){
+        if(two){
             String part[] = s.split("/");
             n = Long.parseLong(part[0]);
             d = Long.parseLong(part[1]);
@@ -27,9 +25,7 @@ public class Rational extends Number implements Comparable<Rational>{
         }
         else
             throw new NumberFormatException();
-//throw new InputMismatchException("InputMismatchException unknown command: " + s);
-        //if(d == 0)
-            //throw new InputMismatchException("InputMismatchException too long: " + s);
+        
         long g = gcd(n, d);
         numerator = (d > 0? 1 : -1) * n / g;
         denominator = Math.abs(d) / g;
